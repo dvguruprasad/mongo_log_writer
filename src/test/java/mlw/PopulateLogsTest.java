8c8d09aa4dec93571a3e6e47d353270c56994c89
Guru <dv.guruprasad@gmail.com>
2013-12-17 14:02:50 -0500

    changing log location and database name

--- src/test/java/mlw/PopulateLogsTest.java
+++ src/test/java/mlw/PopulateLogsTest.java
@@ -9,8 +9,8 @@ import static com.google.common.collect.ImmutableMap.of;
 public class PopulateLogsTest {
     @Test
     public void shouldPopulateLogs() throws IOException {
-        AddToLogs addToLogs = new AddToLogs(MongoDB.create(of("host", "127.0.0.1", "database", "cinnamon")).collection("rp_logs"));
+        AddToLogs addToLogs = new AddToLogs(MongoDB.create(of("host", "127.0.0.1", "database", "foo")).collection("rp_logs"));
         ToLogEntry toLogEntry = new ToLogEntry();
-        new PopulateLogs(addToLogs, toLogEntry).apply("/Users/Thoughtworker/code/kroger/rp_logs/w3-banner_timing_log.11222013");
+        new PopulateLogs(addToLogs, toLogEntry).apply("/Users/Thoughtworker/logs");
     }
 }
