796e36e3318e90ae1f5e31768adc94bc06428872
Guru <dv.guruprasad@gmail.com>
2013-12-17 13:59:22 -0500

    storing timestamp as mongo date

--- src/main/java/mlw/AddToLogs.java
+++ src/main/java/mlw/AddToLogs.java
@@ -4,19 +4,12 @@ import com.google.common.base.Function;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 
-import java.net.UnknownHostException;
-import java.util.Map;
-
 public class AddToLogs implements Function<DBObject, Void> {
+    private final DBCollection rpLogs;
+
     public AddToLogs(DBCollection rpLogs) {
         this.rpLogs = rpLogs;
         this.rpLogs.drop();
-    }
-
-    private final DBCollection rpLogs;
-
-    public AddToLogs(Map<String, String> mongoProperties) throws UnknownHostException {
-        rpLogs = MongoDB.create(mongoProperties).collection("rp_logs");
     }
 
     @Override
--- src/main/java/mlw/ToLogEntry.java
+++ src/main/java/mlw/ToLogEntry.java
@@ -4,6 +4,9 @@ import com.google.common.base.Function;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,23 +14,30 @@ public class ToLogEntry implements Function<String, DBObject> {
     private static Pattern pattern;
 
     static {
-        pattern = Pattern.compile("([\\w\\d.-]+) ([\\d.]+) \\[([\\w\\d:.-]+)\\] \\S [-\\w]+ \\[(\\d{2}/\\w+/\\d{4}:\\d{2}:\\d{2}:\\d{2}\\s[+-]\\d{4})\\] \"(.+?)\"" +
+        pattern = Pattern.compile("([\\w\\d.-]+) ([\\d.]+) \\[([\\w\\d:.-]+)\\] [-\\w]+ [-\\w]+ \\[(\\d{2}/\\w+/\\d{4}:\\d{2}:\\d{2}:\\d{2}\\s[+-]\\d{4})\\] \"(.+?)\"" +
                 "  (\\d{3}):(\\d{3}) ([\\d-]+) \"(.+?)\" .*$");
     }
+
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy:hh:mm:ss zzz");
 
     @Override
     public DBObject apply(String logLine) {
         Matcher matcher = pattern.matcher(logLine);
         if (!matcher.matches())
             throw new RuntimeException("Could not parse " + logLine);
+        Date timestamp = null;
+        try {
+            timestamp = dateFormat.parse(matcher.group(4));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         return new BasicDBObject("domain", matcher.group(1))
                 .append("ip", matcher.group(2))
-                .append("timestamp", matcher.group(4))
+                .append("timestamp", timestamp)
                 .append("request_uri", matcher.group(5))
                 .append("response_status", matcher.group(6))
                 .append("response status_1", matcher.group(7))
                 .append("response_time", matcher.group(8))
                 .append("user_agent", matcher.group(9));
-
     }
 }
