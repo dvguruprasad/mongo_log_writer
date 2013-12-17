5300a1cd903113b9e421fd63eb7053194d74e037
Guru <dv.guruprasad@gmail.com>
2013-12-17 14:04:03 -0500

    changing log location and database name

--- src/main/java/mlw/Main.java
+++ src/main/java/mlw/Main.java
@@ -11,13 +11,13 @@ public class Main {
         MongoDB mongoDB = MongoDB.create(mongoProperties());
         AddToLogs addToLogs = new AddToLogs(mongoDB.collection("rp_logs"));
         ToLogEntry toLogEntry = new ToLogEntry();
-        String logDirectoryPath = fromNullable(System.getProperty("log.dir")).or("/Users/Thoughtworker/code/kroger/rp_logs/w3-banner_timing_log.11222013");
+        String logDirectoryPath = fromNullable(System.getProperty("log.dir")).or("/Users/Thoughtworker/logs");
         new PopulateLogs(addToLogs, toLogEntry).apply(logDirectoryPath);
     }
 
     private static Map<String, String> mongoProperties() {
         return of("host", fromNullable(System.getProperty("host")).or("127.0.0.1"),
-                "database", fromNullable(System.getProperty("database")).or("cinnamon"),
+                "database", fromNullable(System.getProperty("database")).or("foo"),
                 "port", fromNullable(System.getProperty("port")).or(""),
                 "username", fromNullable(System.getProperty("username")).or(""),
                 "password", fromNullable(System.getProperty("password")).or(""));
